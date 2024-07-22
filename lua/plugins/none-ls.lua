979f41c4144d0a4085ce73d706c1329a71b3894f
Nick <nickdeveloper@protonmail.com>
2024-07-22 11:00:05 +0500

    eslint-lsp is better than eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -21,9 +21,9 @@ return {
       null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettierd,
       -- null_ls.builtins.formatting.eslint_d,
-      require "none-ls.formatting.eslint_d",
-      require "none-ls.diagnostics.eslint_d",
-      require "none-ls.code_actions.eslint_d",
+      -- require "none-ls.formatting.eslint_d",
+      -- require "none-ls.diagnostics.eslint_d",
+      -- require "none-ls.code_actions.eslint_d",
       null_ls.builtins.diagnostics.markdownlint,
     })
   end,
